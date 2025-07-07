340f47f6fbf338b0d2eb59ff474d993b6a812476
Usman Malik <usman.malik8015@gmail.com>
2025-07-07 10:09:40 +0300

    Fss Backend2

--- cronEmployeeReminder.js
+++ cronEmployeeReminder.js
@@ -132,7 +132,7 @@ const EXPIRED_ALERT_EMAILS = ["ops1@fssgulf.com", "usmanmunir@fssgulf.com", "usm
 // const EXPIRED_ALERT_EMAILS = ["ops1@fssgulf.com"];
 
 function scheduleEmployeeReminderJob() {
-  cron.schedule("08 10 * * *", async () => {
+  cron.schedule("11 10 * * *", async () => {
     console.log("🔁 Checking employee expiry reminders...");
 
     const snapshot = await db.collection("employees").get();
